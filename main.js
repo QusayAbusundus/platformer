35530408bee4394a8e235fc9f926e0b581b17c62
QusayAbusundus <32073337+QusayAbusundus@users.noreply.github.com>
2017-11-14 13:39:09 -0500

    Added libraries and index.html

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
+let john;
+let platforms = [];
+
 function setup()
 {
-	let john = new Hero(40, 40, 5, 10);
-	let platform = new Platform(60, 40, 80);
+	createCanvas(600, 400);
+	john = new Hero(300, 185);
+	for(i = 0; i < 2; i++)
+	{
+		let x = 260;
+		let y = 190;
+		let width = 80;
+		platforms[i] = new Platform(x, y, width);
+	}
 }
 
+function draw()
+{
+	background(0);
+	john.show();
+	for(i = 0; i < platforms.length; i++)
+	{
+		platforms[i].show();
+	}
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////CLASSES//////////////////////////////////////////////////////////////////////////
@@ -15,8 +34,8 @@ class Hero
 	{
 		this.x = x;
 		this.y = y;
-		this.width = width;
-		this.height = height;
+		this.width = 5;
+		this.height = 10;
 		this.xVelocity = 0;
 		this.yVelocity = 0;
 	}
